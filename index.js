dcebcd6dc60d7e2ea7d6a102e026fd2e81f7177e
bettyje <bettyjelagat156@gmail.com>
2024-10-09 13:05:59 +0300

    Looping

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++){
+        console.log(`wrapped ${gifts[i]} and added a bow!`)
+        debugger;
+    }
+    return gifts;
+}
+wrapGifts(gifts);
+
+function writeCards(names, eventName) {
+    const messages = [];
+
+    for (let i = 0; i < names.length; i++) {
+
+        const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+        
+        messages.push(message);
+    }
+
+    return messages;
+}
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event = "birthday";
+const thankYouCards = writeCards(names, event);
+console.log(thankYouCards);
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
+
+countDown(10);
+
